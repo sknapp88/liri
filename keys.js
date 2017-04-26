c10d38b43339edbfb38723a602352c70957175fc
sknapp88 <knapp533@gmail.com>
2017-04-26 11:52:46 -0400

    initial file creation

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'HkaB18UmmFEHiBdWTGDpUcECX',
+  consumer_secret: 'ELBmJ0eHKSK8HE7xsrDDmxjK90VFD6oqrcRV7P2REdHRuQOnI2',
+  access_token_key: '854906542189105154-pz0D4fKFxrRJUIut4tw7MnaK7jEXaaz',
+  access_token_secret: 'pyDfcFDbDaoBMsfjLPbcmYdVij9htAKfyAtTjPTVEwnZw',
+}
