c6d4e2dd67a5ca39ac216f60b56a744a75c531e2
sknapp88 <knapp533@gmail.com>
2017-04-28 11:13:44 -0400

    added spotify functionality

--- keys.js
+++ keys.js
@@ -5,4 +5,9 @@ exports.twitterKeys = {
   consumer_secret: 'ELBmJ0eHKSK8HE7xsrDDmxjK90VFD6oqrcRV7P2REdHRuQOnI2',
   access_token_key: '854906542189105154-pz0D4fKFxrRJUIut4tw7MnaK7jEXaaz',
   access_token_secret: 'pyDfcFDbDaoBMsfjLPbcmYdVij9htAKfyAtTjPTVEwnZw',
-}
+};
+
+exports.spotifyKeys = {
+	clientID: "4d1f670663f148af9bdb54d3eccd6d44",
+	clientSecret: "c2570f5c64384e33b343b886ce2c4e71"
+};
--- liri.js
+++ liri.js
@@ -1,36 +1,80 @@
-var keys = require("keys.js");
+var keys = require("./keys.js");
 var inquirer = require("inquirer");
 var fs = require("fs");
+var spotify = require("spotify");
 
-var input = process.argv;
-
-function twitter(){
+// var input = process.argv;
+// console.log(input);
+var twitter = function(){
 	if (input[2] === "my-tweets"){
 
 	}
-}
+};
 
-function spotify(){
-	if (input[2] === "spotify-this-song"){
-		inquirer.prompt([
-		{
-			type: "input",
-			message: "What song do you want information about?",
-			name: "song"
+var getArtistNames = function(artist){
+	return artist.name;
+};
+
+var spotifyMe = function(songName){
+	if (songName === undefined){
+		songName = "The Sign";
+	}
+	spotify.search({ 
+		type: 'track',
+		query: songName
+	}, function(err, data) {
+		if ( err ) {
+			console.log('Error occurred: ' + err);
+			return;
 		}
-			]);
-	}
+		var songs = data.tracks.items;
 
-}
+		for (var i = 0; i < songs.length; i++){
+			console.log(i);
+			console.log("artist(s): "+ songs[i].artists.map(getArtistNames));
+			console.log("song name: "+ songs[i].name);
+			console.log("preview song"+ songs[i].preview_url);
+			console.log("album: "+ songs[i].album.name);
+			console.log("----------------------------------");
+		}
+	});
+};
 
-function movie(){
-	if (input[2] === "movie-this"){
 
-	}
-}
 
-function dwis(){
+var movieMe = function(movie){
+	var movieUrl = "http://www.omdbapi.com/?" + movie + "&y"
+};
+
+
+var dwis = function(){
 	if (input[2] === "do-what-it-says"){
 
 	}
-}
+};
+
+var run = function(arg1,arg2){
+	switch(arg1){
+		case "my-tweets": 
+		//to do
+		break;
+
+		case "spotify-this-song":
+		spotifyMe(arg2);
+		break;
+
+		case "movie-this":
+		movieMe(arg2);
+		break;
+
+		case "do-what-it-says":
+		// to do
+		break;
+
+		default: 
+		console.log("invalid input");
+		break;
+	}
+};
+
+run(process.argv[2],process.argv[3]);
